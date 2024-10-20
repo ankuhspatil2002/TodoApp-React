eda1e957f50c0f4e71e7dbda90c9d8dada207c3c
Ankush Patil <ankushpatil2002@gmail.com>
2024-10-20 17:50:49 +0530

    Added Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Navbar.css"
+
+const Navbar = () => {
+  return (
+    <div className="navBar">
+        <div className="logo">
+        Todo App
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
